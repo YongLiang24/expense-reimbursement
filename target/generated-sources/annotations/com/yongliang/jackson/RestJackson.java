ef78afaca1b2c38e5372eb8a0898a1301412af1f
‘yongliang24’ <yongliang24@gmail.com>
2020-03-10 16:10:18 -0400

    implements jackson url reflects the reimbursement table

---
+++ target/generated-sources/annotations/com/yongliang/jackson/RestJackson.java
@@ -0,0 +1,43 @@
+package com.yongliang.jackson;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.yongliang.dbmanipulation.DBManipulationImp;
+import com.yongliang.java.*;
+@Path("/reim")
+public class RestJackson {
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	
+	public ArrayList<Reimbursement> request() {
+		ArrayList<Reimbursement> reimbList = new ArrayList<Reimbursement>();
+		Reimbursement reimb;
+		Connection conn = null;
+		try {
+			 conn = DBConnection.getInstance().getConnection();
+		} catch (SQLException e) {
+		}
+		//select reimbursement information from employee table then add them to an arraylist of employee type
+		String reimbQuery = "select * from reimbursement";
+		DBManipulationImp select = new DBManipulationImp();
+		ResultSet result = select.executeQuery(conn, reimbQuery);
+		try {
+			while(result.next()) {
+				reimb = new Reimbursement(result.getInt(1), result.getInt(2), result.getString(3),result.getString(4),result.getString(5),result.getString(6),result.getString(7),result.getString(8));
+				reimbList.add(reimb);
+			}
+		} catch (SQLException e) {
+		}
+
+		return reimbList;
+	}
+}
---
+++ target/generated-sources/annotations/com/yongliang/jackson/ServletJackson.java
@@ -0,0 +1,43 @@
+package com.yongliang.jackson;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+/**
+ * Servlet implementation class ServletJackson
+ */
+public class ServletJackson extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletJackson() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Client client = ClientBuilder.newClient();
+		WebTarget webTarget = client.target("http://localhost:8080/Project01/rest").path("reim").path("1");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
